b77feb5bf5f9b527038ffa5893fa637665a8c249
ktorres63 <ktorresaro@unsa.edu.pe>
2026-01-28 22:34:05 -0500

    Add Clock animation

--- autoload/GameState.gd
+++ autoload/GameState.gd
@@ -9,12 +9,3 @@ var sospecha_Imperio: int = 0
 var sospecha_Resistencia: int = 0
 
 var mascara_actual: Mascara = Mascara.IMPERIO
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
