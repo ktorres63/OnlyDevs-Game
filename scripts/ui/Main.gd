a7d1ccb4d7ca30753f588023e841a4cea106fc99
ktorres63 <ktorresaro@unsa.edu.pe>
2026-01-30 00:08:45 -0500

    Delete Containers in Main scene

--- scripts/ui/Main.gd
+++ scripts/ui/Main.gd
@@ -3,10 +3,10 @@ extends Control
 @onready var money_label = $HBoxContainer/VBoxContainer3/MoneyLabel
 @onready var turn_label = $HBoxContainer/VBoxContainer2/TurnLabel
 @onready var event_text = $HBoxContainer/VBoxContainer2/EventPanel/EventText
-@onready var phone_red = $HBoxContainer/VBoxContainer/VBoxContainer/PhoneRed
+@onready var phone_red = $HBoxContainer/VBoxContainer/PhoneRed
 @onready var phone_blue = $HBoxContainer/VBoxContainer3/VBoxContainer/PhoneBlue
 @onready var game_manager = $GameManager
-@onready var suspicion_bar_red = $HBoxContainer/VBoxContainer/VBoxContainer/SuspicionBarRed
+@onready var suspicion_bar_red = $HBoxContainer/VBoxContainer/SuspicionBarRed
 @onready var suspicion_bar_blue = $HBoxContainer/VBoxContainer3/VBoxContainer/SuspicionBarBlue
 
 # Called when the node enters the scene tree for the first time.
