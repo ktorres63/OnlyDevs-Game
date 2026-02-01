d27bcfa50b469cfde7107d3c17b034832665d9a1
sllachod <sllachod@unsa.edu.pe>
2026-02-01 18:16:50 -0500

    feature: Agregar panel de créditos y funcionalidad de botones al menú

--- scripts/ui/menu_inicio.gd
+++ scripts/ui/menu_inicio.gd
@@ -2,5 +2,19 @@ extends Control
 
 const JUEGO = "res://scenes/introduction/Intro.tscn"
 
+@onready var panel_creditos = $PanelCreditos
+@onready var menu_botones = $CenterContainer/VBoxContainer
+
 func _on_button_2_button_down():
 	get_tree().change_scene_to_file(JUEGO)
+
+func _on_btn_creditos_pressed():
+	menu_botones.visible = false
+	panel_creditos.visible = true
+
+func _on_btn_cerrar_creditos_pressed():
+	panel_creditos.visible = false
+	menu_botones.visible = true
+
+func _on_btn_salir_pressed():
+	get_tree().quit()
