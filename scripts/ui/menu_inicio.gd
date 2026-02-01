2d622a6d06ad32eb34bfdb335c16455510e3a1df
ktorres63 <ktorresaro@unsa.edu.pe>
2026-02-01 15:25:03 -0500

    Add images and paths

--- scripts/ui/menu_inicio.gd
+++ scripts/ui/menu_inicio.gd
@@ -1,6 +1,6 @@
 extends Control
 
-const JUEGO = "res://scenes/main/Main.tscn"
+const JUEGO = "res://scenes/introduction/Intro.tscn"
 
 func _on_button_2_button_down():
 	get_tree().change_scene_to_file(JUEGO)
